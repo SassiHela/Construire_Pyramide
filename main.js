9a59353989ac7a79c5e6606b780cb4b6eed866e5
SassiHela <hela.sassi13@gmail.com>
2020-02-20 20:23:17 +0100

    Màj de deux fichiers

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-var nbLignes = window.prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"); 
-var char2 = '*'; 
-if (nbLignes <= 25) {
+// Message de saisie
+var nbEtages = window.prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?");
+ 
+var char2 = '*'; // chaine de caractères d'étoiles
+var char1;		// chaine de caractères contenant des espaces 
+if (nbEtages <= 25) { // Affichage de pyramide
+	 
+	for (let i = 1; i <= nbEtages ; i++) {  // Boucle pour parcourir les étages
 	
-	
-	for (let i = 1; i <= nbLignes ; i++) {  // Boucle de lignes
-		var char1 = ' '; 					//chaine contenant les espaces
-		for (let j = nbLignes - i; j > 0 ; j--) {
+		char1 = ' '; 					
+		for (let j = nbEtages - i; j > 0 ; j--) { // Boucle pour la création d'une chaine d'espaces adéquate pour chaque étage.
 			char1+= ' ';
 		}
-		console.log(char1+char2);
-		char2+='**';
-		
-}
+		console.log(char1+char2); 	//affichage d'un étage
+		char2+='**'; 				// chaine d'étoiles de l'étage suivant
+	}
+	
 }else{
+	//Rien faire
 }
 
 	
